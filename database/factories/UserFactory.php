5b953a193bc8f4fabe0225a2d9e219f394e88937
dceballo <darniw@gmail.com>
2018-03-22 03:47:47 -0400

    prueba 2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ $factory->define(avaa\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$rYXRkrnr5g74uxCGZGnioO/ZQMHr8xlGfz/cVsMtWEQoM6atMC2TS', // secret
         'remember_token' => str_random(10),
     ];
 });
