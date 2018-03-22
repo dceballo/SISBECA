d312ded172140d5d871977e6857a34e667002c4b
Yeselimar <ilimar.vasquez@gmail.com>
2018-03-22 04:14:54 -0400

    Revert " prueba"
    This reverts commit c0145324c4a8bf568e4ce0432d7de48797b1a5e8.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,7 +17,7 @@ $factory->define(avaa\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$pOVcprZIVKUvlveOagvYPuUmrMdj6a8wUMIBLZyziemw6bzRwtqa2', // secret
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
 });
